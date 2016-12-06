f47826fd3d3c2ce778b6c7c82ec0afcb0ba07e46
TomIrimescu <tom@irimescu.com>
2016-12-06 15:42:50 -0500

    Code cleanup, added stock.json data source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,58 +21,17 @@ export class AppComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription;
 
-  public constructor( private stockService: StockService){
+  public constructor( private stockService: StockService ){
     this.Portfolio = [];
   }
 
   public  ngOnInit () {
-
-    sessionStorage.setItem('cash', '100000.13');
+    sessionStorage.setItem('cash', '100000.00');
     this.cash = parseFloat(sessionStorage.getItem('cash'));
-    this.cash = ((this.cash)-(.13));
-    console.log(typeof (this.cash)); /*testing*/
-    console.log(this.cash); /*testing*/
-
-/*    this.Portfolio.push(
-        {
-          stock: {
-            symbol: 'F',
-            name: 'Ford Motor',
-            bidPrice: 11.92,
-            askPrice: 11.93
-          },
-          "quantity": 13000,
-          "value": 13000
-        },
-
-        {
-          stock: {
-            symbol: 'GE',
-            name: 'General Electric',
-            bidPrice: 20.00,
-            askPrice: 21.93
-          },
-          "quantity": 50,
-          "value": 1000
-        },
-
-        {
-          stock: {
-            symbol: 'MSFT',
-            name: 'Microsoft',
-            bidPrice: 20.00,
-            askPrice: 21.93
-          },
-          "quantity": 5,
-          "value": 24.99
-        }
-    );*/
 
     this.subscription = this.stockService.stockChanged.subscribe(
         (stock: Stock) => this.stock = stock
     );
-
-    console.log(this.stock); /*testing*/
 
   }
 
@@ -125,50 +84,34 @@ export class AppComponent implements OnInit, OnDestroy {
     } else {
       document.getElementById("error-message").innerHTML = '';
       if ((this.cash >= stockQuantity * ASK) && (stockQuantity > 0)) {
-        console.log(this.Portfolio.map(this.getSymbols));/*testing*/
-        console.log(this.Portfolio.map(this.getQuantities));/*testing*/
-        console.log(this.Portfolio.map(this.getValues));/*testing*/
         var buySymbols = this.Portfolio.map(this.getSymbols);
         var buyQuantities = this.Portfolio.map(this.getQuantities);
         var buyValues = this.Portfolio.map(this.getValues);
         var arrayLength = buySymbols.length;
         for (var i = 0; i < arrayLength; i++) {
-          console.log(buySymbols[ i ]);/*testing*/
-          console.log(buyQuantities[ i ]);/*testing*/
-          console.log(buyValues[ i ]);/*testing*/
           if (buySymbols[ i ] == stockSym) {
-            console.log(stockSym);/*testing*/
-            console.log('This is a stock update');/*testing*/
-            console.log(this.Portfolio[ i ].stock.symbol);/*testing*/
-
             var totalQuantity: number = parseFloat(stockQuantity) + parseFloat(buyQuantities[ i ]);
-            console.log('Quantity total: ' + totalQuantity);/*testing*/
             var totalValue: number = parseFloat((stockQuantity * ASK) + (buyValues[ i ]));
-            console.log('Value total: ' + totalValue);/*testing*/
             this.Portfolio[ i ] = new Holding(new Stock(stockSym, stockName, BID, ASK), totalQuantity, totalValue);
-
             this.cash = (this.cash) - (stockQuantity * ASK);
-            console.log(typeof (this.cash));/*testing*/
             document.getElementById("message").innerHTML = 'Purchased: ' + stockQuantity + ' ' + stockName + ' stock for ' + (stockQuantity * ASK);
             var quantityElement = <HTMLInputElement>document.getElementById("quantityStock");
             quantityElement.value = '';
             return;
           }
         }
-        console.log('This is a new stock purchase'); /*testing*/
         this.Portfolio.push(new Holding(new Stock(stockSym, stockName, BID, ASK), stockQuantity, (stockQuantity * ASK)));
         this.cash = (this.cash) - (stockQuantity * ASK);
-        console.log(typeof (this.cash));/*testing*/
         document.getElementById("message").innerHTML = 'Purchased: ' + stockQuantity + ' ' + stockName + ' stock for ' + (stockQuantity * ASK);
         var quantityElement = <HTMLInputElement>document.getElementById("quantityStock");
         quantityElement.value = '';
 
-      } else if(stockQuantity == 0) {
+      } else if (stockQuantity == 0) {
         document.getElementById("message").innerHTML = '<span style="color: lightsalmon;">Select a quantity</span>';
         var quantityElement = <HTMLInputElement>document.getElementById("quantityStock");
         quantityElement.value = '';
         return;
-      } else{
+      } else {
         document.getElementById("message").innerHTML = '<span style="color: lightsalmon;">You will exceed <br>your' +
             ' available cash!</span>';
         var quantityElement = <HTMLInputElement>document.getElementById("quantityStock");
@@ -191,34 +134,17 @@ export class AppComponent implements OnInit, OnDestroy {
     } else {
       document.getElementById("message").innerHTML = '';
       document.getElementById("error-message").innerHTML = '';
-/*    console.log(this.Portfolio.map(this.getSymbols)); /!*testing*!/
-      console.log(this.Portfolio.map(this.getQuantities)); /!*testing*!/
-      console.log(this.Portfolio.map(this.getValues)); /!*testing*!/*/
       var sellSymbols = this.Portfolio.map(this.getSymbols);
       var sellQuantities = this.Portfolio.map(this.getQuantities);
       var sellValues = this.Portfolio.map(this.getValues);
       var arrayLength = sellSymbols.length;
       for (var i = 0; i < arrayLength; i++) {
-/*        console.log(sellSymbols[ i ]);/!*testing*!/
-        console.log(sellQuantities[ i ]);/!*testing*!/
-        console.log(sellValues[ i ]);/!*testing*!/*/
         if (sellSymbols[ i ] == stockSym) {
-/*          console.log(stockName); /!*testing*!/
-          console.log(stockSym); /!*testing*!/
-          console.log(stockQuantity); /!*testing*!/
-          console.log('This is current stock sale'); /!*testing*!/
-          console.log(this.Portfolio[i].stock.symbol); /!*testing*!/*/
-
           if((sellQuantities[ i ]) >= stockQuantity){
               var totalQuantity: number = parseFloat(sellQuantities[ i ]) - parseFloat(stockQuantity);
-/*              console.log('Quantity total: ' + totalQuantity);/!*testing*!/*/
               var totalValue: number = (sellValues[ i ]) - (stockQuantity * BID);
-/*              console.log('Value total: ' + totalValue);/!*testing*!/*/
-
               this.Portfolio[ i ] = new Holding(new Stock(stockSym, stockName, BID, ASK), totalQuantity, totalValue);
-
               this.cash = (this.cash) + (stockQuantity * BID);
-/*              console.log(typeof (this.cash));/!*testing*!/*/
               document.getElementById("message").innerHTML = 'Sold: ' + stockQuantity + ' ' + stockName + ' stock for ' + (stockQuantity * BID);
               var quantityElement = <HTMLInputElement>document.getElementById("quantityStock");
               quantityElement.value = '';
@@ -236,6 +162,5 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy(){
     this.subscription.unsubscribe();
   }
-
 
 }
--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -13,15 +13,12 @@ export class StockService {
   constructor(private http: Http) { }
 
   symbolLookup(symbol: string){
-    console.log(symbol); /*testing*/
-    console.log('I did a symbol lookup'); /*testing*/
-
-      return this.http.get('http://data.benzinga.com/rest/richquoteDelayed?symbols=' + symbol)
-      .map((response: Response) => response.json()[ symbol ])
+/*  return this.http.get('http://data.benzinga.com/rest/richquoteDelayed?symbols=' + symbol)*/
+      return this.http.get('stocks.json')
+      .map((response: Response) => response.json()[symbol])
       .subscribe(
           (data: any) => {
             this.stock = data;
-            console.log(this.stock); /*testing*/
             if (this.stock === undefined || symbol === 'null') {
               document.getElementById("error-message").innerHTML = 'Unknown symbol';
             } else {
@@ -33,15 +30,12 @@ export class StockService {
   }
 
   viewLookup(symbol: string){
-    console.log(symbol); /*testing*/
-    console.log('I did a view lookup'); /*testing*/
-
-    return this.http.get('http://data.benzinga.com/rest/richquoteDelayed?symbols=' + symbol)
+/*  return this.http.get('http://data.benzinga.com/rest/richquoteDelayed?symbols=' + symbol)*/
+    return this.http.get('stocks.json')
     .map((response: Response) => response.json()[symbol])
     .subscribe(
         (data: any) => {
           this.stock = data;
-          console.log(this.stock); /*testing*/
           this.stockChanged.emit(this.stock);
         }
     );
